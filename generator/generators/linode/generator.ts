9ebfb51e5be77d5e47bda100a81945cb63e2e842
Partik SIngh <partik.sbumrah.cse21@itbhu.ac.in>
2023-04-07 11:47:55 +0530

    Linode
    Signed-off-by: Partik SIngh <partik.sbumrah.cse21@itbhu.ac.in>

--- generator/generators/linode/generator.ts
+++ generator/generators/linode/generator.ts
@@ -1,57 +1,54 @@
-import { getAST } from "../../parsers/linode/parser";
+import { getAST } from '../../parsers/linode/parser';
 
 interface FunctionData {
-  pkgName: string;
-  fileName: string;
-  functionName: string;
-  SDKFunctionName: string;
-  params: param[];
-  returnType: string;
-  client: string;
+	pkgName: string;
+	fileName: string;
+	functionName: string;
+	SDKFunctionName: string;
+	params: param[];
+	returnType: string;
+	client: string;
 }
 
 interface param {
-  name: string;
-  type: string;
+	name: string;
+	type: string;
 }
 
-
 export async function generateLinodeClass(serviceClass, serviceName) {
-  let methods: FunctionData[] = [];
-    console.log("serviceClass", serviceClass);
-    
-  Object.keys(serviceClass).map((key, index) => {
-    methods.push({
-      pkgName: serviceClass[key].split(" ")[0],
-      fileName: serviceClass[key].split(" ")[1],
-      functionName: key,
-      SDKFunctionName: serviceClass[key].split(" ")[2],
-      params: [],
-      returnType: null,
-      client: null
-    });
-  });
+	let methods: FunctionData[] = [];
+	console.log('serviceClass', serviceClass);
 
-  const files = Array.from(new Set(methods.map(method => method.fileName)));
+	Object.keys(serviceClass).map((key, index) => {
+		methods.push({
+			pkgName: serviceClass[key].split(' ')[0],
+			fileName: serviceClass[key].split(' ')[1],
+			functionName: key,
+			SDKFunctionName: serviceClass[key].split(' ')[2],
+			params: [],
+			returnType: null,
+			client: null,
+		});
+	});
 
-  const sdkFiles = files.map(file => {
-    return {
-      fileName: file,
-      pkgName: methods[0].pkgName,
-      ast: null,
-      client: null,
-      sdkFunctionNames: methods
-        .filter(method => method.fileName === file)
-        .map(method => method.SDKFunctionName)
-    };
-  });
-  console.log("sdkFiles", sdkFiles);
-  
+	const files = Array.from(new Set(methods.map(method => method.fileName)));
 
-  await Promise.all(
-    sdkFiles.map(async file => {
-        await getAST(file);
-    })
-  ); 
+	const sdkFiles = files.map(file => {
+		return {
+			fileName: file,
+			pkgName: methods[0].pkgName,
+			ast: null,
+			client: null,
+			sdkFunctionNames: methods
+				.filter(method => method.fileName === file)
+				.map(method => method.SDKFunctionName),
+		};
+	});
+	console.log('sdkFiles', sdkFiles);
 
+	await Promise.all(
+		sdkFiles.map(async file => {
+			await getAST(file);
+		})
+	);
 }
