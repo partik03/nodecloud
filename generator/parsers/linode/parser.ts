d73fd85663e12f0297cdd633aca6b87e0b21f39d
Partik SIngh <partik.sbumrah.cse21@itbhu.ac.in>
2023-06-11 22:54:20 +0530

    test
    Signed-off-by: Partik SIngh <partik.sbumrah.cse21@itbhu.ac.in>

--- generator/parsers/linode/parser.ts
+++ generator/parsers/linode/parser.ts
@@ -20,9 +20,12 @@ export function getAST(sdkFileInfo) {
 			);
 
 			let cloned = null;
-
+            
 			await ast.forEachChild(child => {
-				console.log('Linode', SyntaxKind[child.kind]);
+                fs.writeFile("test.txt",SyntaxKind[child.kind],null)
+                // console.log(SyntaxKind[child.kind]);
+                
+				// console.log('Linode', SyntaxKind[child.kind]);
 
 				if (SyntaxKind[child.kind] === 'FirstStatement') {
 					cloned = Object.assign({}, child);
