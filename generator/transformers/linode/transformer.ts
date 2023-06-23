639acf01a373f108b66a36af4ac49c66c1e8339f
Partik SIngh <partik.sbumrah.cse21@itbhu.ac.in>
2023-06-23 21:04:09 +0530

    Added transformer for linode
    Signed-off-by: Partik SIngh <partik.sbumrah.cse21@itbhu.ac.in>

---
+++ generator/transformers/linode/transformer.ts
@@ -0,0 +1,42 @@
+import ts = require("typescript");
+
+export async function transform(code:ts.SourceFile,classData:any) :Promise<string> {
+
+    const addFunctions = <T extends ts.Node>(
+        context : ts.TransformationContext
+    )=>(rootNode:T) =>{
+        function visit(node:ts.Node):ts.Node {
+            if(ts.isClassDeclaration(node)){
+                let functions:any =[]
+                classData.functions.map(method=>{
+                    const clonedNode = Object.assign({},node.members[1]);
+
+                    clonedNode.name = ts.createIdentifier(method.functionName);
+                    functions.push(clonedNode);
+                })
+
+                const updatedClass = ts.updateClassDeclaration(
+                    node,
+                    node.decorators,
+                    node.name,
+                    node.typeParameters,
+                    node.heritageClauses,
+                    ts.createNodeArray([node.members[0]].concat(functions)),
+                );
+
+                return updatedClass;
+            }
+            return ts.visitEachChild(node,visit,context);
+        }
+        return ts.visitNode(rootNode,visit)
+    };
+
+    const addIdentifiers =<T extends ts.Node>(
+        context: ts.TransformationContext
+    ) => (rootNode :T) =>{
+        let count =0;
+        
+    }
+
+
+}
